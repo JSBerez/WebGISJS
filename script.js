eec5c73a2e6d723aac9d140c5218d647d58df4a1
JSBerez <59605475+JSBerez@users.noreply.github.com>
2020-01-29 19:25:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-require (["esri/WebMap","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/BasemapGallery", "esri/widgets/Expand"], //tutaj pobieramy Moduł z arcgis api for js
-      function(WebMap, Map, MapView, FeatureLayer, BasemapGallery, Expand) //tutaj wiążemy Moduł z funkcją
+require (["esri/WebMap", "esri/Map", "esri/views/MapView", "esri/layers/GraphicsLayer", "esri/widgets/BasemapGallery", "esri/widgets/Expand", "esri/widgets/Sketch", "esri/layers/FeatureLayer"], //tutaj pobieramy Moduł z arcgis api for js
+      function(WebMap, Map, MapView, GraphicsLayer, BasemapGallery, Expand, Sketch, FeatureLayer) //tutaj wiążemy Moduł z funkcją
       {
+		
+		let graphicsLayer = new GraphicsLayer();
+
 		let webmap = new WebMap({
 			portalItem: {
 			  id: "131eb8b1a13640ddbb38049cd88941c1"
-			}
+			},
+			layers: [graphicsLayer]
 		});
-//         let map1 = new Map({basemap:"topo"}); //słowo klucz new + nazwa modułu
-        let mapContainer = new MapView({ // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html
+
+/*
+		let map1 = new Map({
+			basemap:"topo",
+			layers: [graphicsLayer]
+		}); //słowo klucz new + nazwa modułu
+*/
+
+		let mapContainer = new MapView({ // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html
 			container: "viewDiv", //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej
 			map: webmap,
+//			map: map1,
             zoom: 6, // ustawienia zoomu
             center: [-1.16,52.61]
         });
 
-		let template = {
-			title: "{Football_Stadiums_of_England_and_Wales}",
-			content: [
-				{
-					type: "fields",
-					fieldInfos: [
-						{
-							fieldName: "Division",
-							label: "Poziom Rozgrywek"
-						},
-						{
-							fieldName: "Team",
-							label: "Zespół"
-						},
-						{
-							fieldName: "Location",
-							label: "Miejscowość"
-						},
-						{
-							fieldName: "Stadium",
-							label: "Stadion"
-						},
-						{
-							fieldName: "Year_Open",
-							label: "Rok Otwarcia"
-						},
-						{
-							fieldName: "Capacity",
-							label: "Pojemność"
-						},
-					]
-				}
-			]
-		};
+		let trailheadsLayer = new FeatureLayer({
+			url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/USA%20States/FeatureServer/0"
+		});
+	
+		webmap.add(trailheadsLayer);
+
+
+		let sketch = new Sketch({
+			view: mapContainer,
+			layer: graphicsLayer
+		  });
+	
+		mapContainer.ui.add(sketch, "top-right");
 		
 		
         // dodanie widgetu map bazowych i jego minimalizowanie
